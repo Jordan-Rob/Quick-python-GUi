6bbc897fcc33dc694acf5094b084237044b63f8e
jordanrob709 <jordanrob709@gmail.com>
2019-11-08 19:20:17 +0300

    initial commit

--- part_manager.py
+++ part_manager.py
@@ -5,7 +5,7 @@ app = Tk()
 
 #part
 part_text = StringVar()
-part_label = Label(app, text='part Name', font=('bold', 14), pady=20)
+part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
 part_label.grid(row=0, column=0)
 
 part_entry = Entry(app, width=25)

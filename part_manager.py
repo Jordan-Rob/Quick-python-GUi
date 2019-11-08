ab8536c7fc7f4f0496f8a3acd1f92db7a9e1c945
jordanrob709 <jordanrob709@gmail.com>
2019-11-08 19:44:08 +0300

    5th commit

--- part_manager.py
+++ part_manager.py
@@ -19,6 +19,23 @@ customer_label.grid(row=0, column=2)
 customer_entry = Entry(app, width=25, textvariable=customer_text)
 customer_entry.grid(row=0, column=3)
 
+# Retailer
+Retailer_text = StringVar()
+Retailer_label = Label(app, text="Retailer", font=('bold', 14))
+Retailer_label.grid(row=1, column=0)
+
+Retailer_entry = Entry(app, width=25, textvariable=Retailer_text)
+Retailer_entry.grid(row=1, column=1)
+
+
+# Price
+Price_text = StringVar()
+Price_label = Label(app, text="Price", font=('bold', 14))
+Price_label.grid(row=1, column=2)
+
+Price_entry = Entry(app, width=25, textvariable=Price_text)
+Price_entry.grid(row=1, column=3)
+
 app.title('Part Manager')
 app.geometry('700x350')
 

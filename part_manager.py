1d95696a466859d75c306aa2f3776f1a6d2402e0
jordanrob709 <jordanrob709@gmail.com>
2019-11-08 19:31:53 +0300

    4th commit

--- part_manager.py
+++ part_manager.py
@@ -11,6 +11,14 @@ part_label.grid(row=0, column=0)
 part_entry = Entry(app, width=25, textvariable=part_text)
 part_entry.grid(row=0, column=1)
 
+# customer
+customer_text = StringVar()
+customer_label = Label(app, text="Customer", font=('bold', 14))
+customer_label.grid(row=0, column=2)
+
+customer_entry = Entry(app, width=25, textvariable=customer_text)
+customer_entry.grid(row=0, column=3)
+
 app.title('Part Manager')
 app.geometry('700x350')
 

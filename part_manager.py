fa6bfabd96f4ef5295d48b7ed92b118472cdd438
jordanrob709 <jordanrob709@gmail.com>
2019-11-08 19:26:32 +0300

    3rd commit

--- part_manager.py
+++ part_manager.py
@@ -1,14 +1,14 @@
 from tkinter import *
 
-#create a window object
+# create a window object
 app = Tk()
 
-#part
+# part
 part_text = StringVar()
 part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
 part_label.grid(row=0, column=0)
 
-part_entry = Entry(app, width=25)
+part_entry = Entry(app, width=25, textvariable=part_text)
 part_entry.grid(row=0, column=1)
 
 app.title('Part Manager')
